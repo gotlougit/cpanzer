3bb1d6dee048ca87cd4f9abea8df83e3c57b7d3f
Saksham Mittal <gotlougit@users.noreply.github.com>
2021-11-05 12:04:06 +0530

    initial commit

---
+++ Makefile
@@ -0,0 +1,41 @@
+# A simple Makefile for compiling small SDL projects
+
+# set the compiler
+CC := gcc 
+
+# set the compiler flags
+CFLAGS := `sdl2-config --libs --cflags` -ggdb3 -O0 --std=c99 -Wall -lSDL2_image -lm
+# add header files here
+HDRS :=
+
+# add source files here
+SRCS := window.c #file-name.c
+
+# generate names of object files
+OBJS := $(SRCS:.c=.o)
+
+# name of executable
+EXEC := main#name your executable file
+
+# default recipe
+all: $(EXEC)
+ 
+showfont: showfont.c Makefile
+	$(CC) -o $@ $@.c $(CFLAGS) $(LIBS)
+
+glfont: glfont.c Makefile
+	$(CC) -o $@ $@.c $(CFLAGS) $(LIBS)
+
+# recipe for building the final executable
+$(EXEC): $(OBJS) $(HDRS) Makefile
+	$(CC) -o $@ $(OBJS) $(CFLAGS)
+
+# recipe for building object files
+#$(OBJS): $(@:.o=.c) $(HDRS) Makefile
+#    $(CC) -o $@ $(@:.o=.c) -c $(CFLAGS)
+
+# recipe to clean the workspace
+clean:
+	rm -f $(EXEC) $(OBJS)
+
+.PHONY: all clean
---
+++ window.c
@@ -0,0 +1,117 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+#define WIDTH 800
+#define HEIGHT 800
+#define FRAMERATE 60
+#define IMAGESCALE 6
+#define TEXTURE_LOC "img.jpg"
+
+const int speed = 300;
+int close = 0;
+
+int main(int argc, char *argv[]) {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("Error initializing SDL: %s\n", SDL_GetError());
+	}
+	SDL_Window *win = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+
+	uint32_t render_flags = SDL_RENDERER_ACCELERATED;
+	SDL_Renderer *rend = SDL_CreateRenderer(win, -1, render_flags);
+
+	SDL_Surface *surface;
+	surface = IMG_Load(TEXTURE_LOC);
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, surface);
+
+	SDL_FreeSurface(surface);
+
+	SDL_Rect dest, plain;
+	SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+	SDL_QueryTexture(tex, NULL, NULL, &plain.w, &plain.h);
+
+	plain.w = plain.w / (2*IMAGESCALE);
+	plain.h = plain.h / (2*IMAGESCALE);
+	plain.x = (WIDTH-plain.w)/4;
+	plain.y = (HEIGHT-plain.h)/4;
+
+	dest.w = dest.w/IMAGESCALE;
+	dest.h = dest.h/IMAGESCALE;
+	dest.x = (WIDTH-dest.w)/2;
+	dest.y = (HEIGHT-dest.h)/2;
+
+	while (!close) {
+		
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			
+			switch (event.type) {
+				
+				case SDL_QUIT:
+					close = 1;
+					break;
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.scancode) {
+						case SDL_SCANCODE_Q:
+							close = 1;
+							break;
+						case SDL_SCANCODE_W:
+						case SDL_SCANCODE_UP:
+							dest.y -= speed / (FRAMERATE/2);
+							break;
+						case SDL_SCANCODE_A:
+						case SDL_SCANCODE_LEFT:
+							dest.x -= speed / (FRAMERATE/2);
+							break;
+						case SDL_SCANCODE_S:
+						case SDL_SCANCODE_DOWN:
+							dest.y += speed / (FRAMERATE/2);
+							break;
+						case SDL_SCANCODE_D:
+						case SDL_SCANCODE_RIGHT:
+							dest.x += speed / (FRAMERATE/2);
+							break;
+						default:
+							break;
+					}
+					break;
+
+
+			}
+
+		}
+	
+	if (dest.x + dest.w > WIDTH) {
+		dest.x = WIDTH - dest.w;
+	}
+
+	if (dest.x < 0) {
+		dest.x = 0;
+	}
+
+	if (dest.y + dest.h > HEIGHT) {
+		dest.y = HEIGHT - dest.h;
+	}
+	
+	if (dest.y < 0) {
+		dest.y = 0;
+	}
+
+	SDL_RenderClear(rend);
+	SDL_RenderCopy(rend, tex, NULL, &dest);
+	SDL_RenderCopy(rend, tex, NULL, &plain);
+
+	SDL_RenderPresent(rend);
+	SDL_Delay(WIDTH/FRAMERATE);
+
+	}
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(rend);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	return 0;
+}
